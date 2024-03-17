b8fcb50065da51ffdce269ea32403a423b2dcc4a
ConicalDrupe <ChrisJacobBoon@gmail.com>
2024-03-16 22:30:02 -0400

    finished Lazy transition

--- lua/boon/core/keymaps.lua
+++ lua/boon/core/keymaps.lua
@@ -41,23 +41,23 @@ keymap.set("n", "<leader>tj", ":tabp<CR>") --  go to previous tab
 -- plugin keymaps
 
 -- vim-maximizer
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+-- keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 
 -- NvimToggleTree keymap
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+-- keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 
 -- telescope, ff finds files, fs finds text in file, fc finds txt on current string
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+-- keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+-- keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+-- keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+-- keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+-- keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
 
 -- tmux-nvim navigator remap
-keymap.set("n","<S-h>", ":<C-U>TmuxNavigateLeft<cr>")
-keymap.set("n", "<S-j>", ":<C-U>TmuxNavigateDown<cr>")
-keymap.set("n", "<S-k>", ":<C-U>TmuxNavigateUp<cr>")
-keymap.set("n", "<S-l>", ":<C-U>TmuxNavigateRight<cr>")
+-- keymap.set("n","<S-h>", ":<C-U>TmuxNavigateLeft<cr>")
+-- keymap.set("n", "<S-j>", ":<C-U>TmuxNavigateDown<cr>")
+-- keymap.set("n", "<S-k>", ":<C-U>TmuxNavigateUp<cr>")
+-- keymap.set("n", "<S-l>", ":<C-U>TmuxNavigateRight<cr>")
 --keymaps.set("n", "{Previous-Mapping}" ":<C-U>TmuxNavigatePrevious<cr>")
 
 -- nvim-jupyter kit remap
