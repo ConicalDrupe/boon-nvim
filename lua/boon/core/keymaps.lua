2c69bda5bdff74fdd958fb51fb8892526aca5145
ConicalDrupe <ChrisJacobBoon@gmail.com>
2024-03-28 19:25:34 -0400

    Added descriptions for quarto code chunks for fzf via Telescope

--- lua/boon/core/keymaps.lua
+++ lua/boon/core/keymaps.lua
@@ -46,8 +46,8 @@ vim.keymap.set('n', '<S-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- Quarto and slime Keymaps
 -- cannot use <C-i>, which is equivalant to <Tab> (like <C-]> is equivalent to <Esc>)
-vim.keymap.set({'n', 'i'}, '<leader>ic','<esc>i<cr>```{python}<cr>```<esc>0' ,{desc = '[i]nsert [c]ode chunk'})
-vim.keymap.set({'n', 'i'}, '<leader>iC','<esc>i<cr>```{r}<cr>```<esc>0' ,{desc = '[i]nsert [c]ode chunk'})
+vim.keymap.set({'n', 'i'}, '<leader>ic','<esc>i<cr>```{python}<cr>```<esc>0' ,{desc = '[i]nsert python [c]ode chunk'})
+vim.keymap.set({'n', 'i'}, '<leader>iC','<esc>i<cr>```{r}<cr>```<esc>0' ,{desc = '[i]nsert R [c]ode chunk'})
 
 -- Create a terminal and then Navigate to Previous Window
 vim.keymap.set('n', '<leader>cr',':split term://ipython<cr> <bar> <C-w><C-p>' ,{desc = '[c]reate ipython [r]epl'})
