37ce5ace04bcd9b871a2b5760ab538aaac4c68e5
ConicalDrupe <ChrisJacobBoon@gmail.com>
2024-04-03 16:49:39 -0400

    Added keymaps for jumping, and added snippets path

--- lua/boon/plugins/luasnip.lua
+++ lua/boon/plugins/luasnip.lua
@@ -9,10 +9,14 @@ return {
           end
           return 'make install_jsregexp'
         end)(),
+
         dependencies = {
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
+
+          -- Add snippet path below
+
           {
             'rafamadriz/friendly-snippets',
             config = function()
@@ -20,4 +24,37 @@ return {
             end,
           },
         },
+        opts = {
+          history = true,
+          updateevents = "TextChanged,TextChangedI",
+          enable_autosnippets = true,
+        },
+        config = function(_,opts)
+          require('luasnip').setup(opts)
+          require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/snippets"})
+          local ls = require('luasnip')
+
+          -- Jump forward in snippet
+          vim.keymap.set({ 'i', 's'}, '<C-k>', function ()
+            if ls.expand_or_jumpable() then
+              ls.expand_or_jump()
+            end
+          end, {silent = true})
+
+          vim.keymap.set({ 'i', 's'}, '<C-j>', function ()
+            if ls.jumpable(-1) then
+              ls.jump(-1)
+            end
+          end, {silent = true})
+
+          vim.keymap.set({ 'i', 's'}, '<C-l>', function ()
+            if ls.choice_active() then
+              ls.choice_active(1)
+            end
+          end, {silent = true})
+
+          vim.keymap.set('n', '<leader><leader>s', '<cmd>source ~/.config/nvim/lua/boon/plugins/luasnip.lua<CR>')
+      end,
+
       }
+
