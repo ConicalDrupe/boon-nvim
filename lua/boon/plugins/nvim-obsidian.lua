9493a7969e43fb767d1078eec80aa67688949bbe
ConicalDrupe <ChrisJacobBoon@gmail.com>
2024-04-03 15:13:56 -0400

    Added barebones functionality, now to add keymaps

--- lua/boon/plugins/nvim-obsidian.lua
+++ lua/boon/plugins/nvim-obsidian.lua
@@ -1,33 +1,78 @@
 return {
   "epwalsh/obsidian.nvim",
   version = "*",  -- recommended, use latest release instead of latest commit
-  lazy = true,
-  ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
-  -- },
+  -- Only loads when markdown is opened
+  -- lazy = true,
+  -- ft = "markdown",
   dependencies = {
-    -- Required.
     "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies 👇
   },
   opts = {
-    workspaces = {
-      {
-        name = "Notes_2024",
-        path = "~/Vaults/Notes_2024",
-      },
-      -- {
-      --   name = "work",
-      --   path = "~/vaults/work",
-      -- },
+  workspaces = {
+    -- {
+    --   name = "Notes_2024",
+    --   path = "~/Vaults/Notes_2024",
+    -- },
+    {
+      name = "Notes_2024",
+      path = "/home/boon/Vaults/Notes_2024",
     },
-
-    -- see below for full list of options 👇
+    {
+      name = "localVault",
+      path = "/home/boon/Vaults/localVault",
+      -- `strict=true` here tells obsidian to use the `path` as the workspace/vault root,
+      -- even though the actual Obsidian vault root may be `~/vaults/personal/`.
+      strict = true,
+      overrides = {
+        -- ...
+      },
   },
+    },
+  completion = {
+    nvim_cmp = true,
+    min_chars = 2,
+  },
+
+  note_frontmatter_func = function(note)
+    -- This is equivalent to the default frontmatter function.
+    local out = { id = note.id, aliases = note.aliases, tags = note.tags, area = "", project = "" }
+
+    -- `note.metadata` contains any manually added fields in the frontmatter.
+    -- So here we just make sure those fields are kept in the frontmatter.
+    if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+      for k, v in pairs(note.metadata) do
+        out[k] = v
+      end
+    end
+    return out
+  end,
+
+  note_id_func = function(title)
+    -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+    -- In this case a note with the title 'My new note' will be given an ID that looks
+    -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+    local suffix = ""
+    if title ~= nil then
+      -- If title is given, transform it into valid file name.
+      suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+    else
+      -- If title is nil, just add 4 random uppercase letters to the suffix.
+      for _ = 1, 4 do
+        suffix = suffix .. string.char(math.random(65, 90))
+      end
+    end
+    return tostring(os.time()) .. "-" .. suffix
+  end,
+
+  templates = {
+      subdir = "Templates",
+      date_format = "%Y-%m-%d-%a",
+      time_format = "%H:%M",
+      tags = "",
+  },
+  },
+  config = function (_, opts)
+    vim.opt.conceallevel = 1
+    require('obsidian').setup(opts)
+  end
 }
