65400077cdad7fd7e3088632ff0f4723acbc6684
ConicalDrupe <ChrisJacobBoon@gmail.com>
2024-04-18 21:01:29 -0400

    Tried to get python to call from R repl.... Try to put vim slime config within quarto dependency

--- lua/boon/core/keymaps.lua
+++ lua/boon/core/keymaps.lua
@@ -51,6 +51,7 @@ vim.keymap.set({'n', 'i'}, '<leader>iC','<esc>i<cr>```{r}<cr>```<esc>0' ,{desc =
 
 -- Create a terminal and then Navigate to Previous Window
 vim.keymap.set('n', '<leader>cr',':split term://ipython<cr> <bar> <C-w><C-p>' ,{desc = '[c]reate ipython [r]epl'})
+vim.keymap.set('n', '<leader>cR',':split term://R --no-save<cr> <bar> <C-w><C-p>' ,{desc = '[c]reate ipython [r]epl'})
 
 -- Open my Obsian in current buffer
 -- vim.keymap.set('n', '<C-n>', '<cmd>edit ~/Vaults/Notes_2024/<cr>')
@@ -105,4 +106,55 @@ end)
 --     end
 --     vim.cmd('normal' .. slime_send_region_cmd)
 --   end
--- end
+--
+vim.g['quarto_is_r_mode'] = nil
+vim.g['reticulate_running'] = false
+
+local function send_cell()
+  if vim.b['quarto_is_r_mode'] == nil then
+    vim.fn['slime#send_cell']()
+    return
+  end
+  if vim.b['quarto_is_r_mode'] == true then
+    vim.g.slime_python_ipython = 0
+    local is_python = require('otter.tools.functions').is_otter_language_context 'python'
+    if is_python and not vim.b['reticulate_running'] then
+      vim.fn['slime#send']('reticulate::repl_python()' .. '\r')
+      vim.b['reticulate_running'] = true
+    end
+    if not is_python and vim.b['reticulate_running'] then
+      vim.fn['slime#send']('exit' .. '\r')
+      vim.b['reticulate_running'] = false
+    end
+    vim.fn['slime#send_cell']()
+  end
+end
+
+--- Send code to terminal with vim-slime
+--- If an R terminal has been opend, this is in r_mode
+--- and will handle python code via reticulate when sent
+--- from a python chunk.
+local slime_send_region_cmd = ':<C-u>call slime#send_op(visualmode(), 1)<CR>'
+slime_send_region_cmd = vim.api.nvim_replace_termcodes(slime_send_region_cmd, true, false, true)
+local function send_region()
+  -- if filetyps is not quarto, just send_region
+  if vim.bo.filetype ~= 'quarto' or vim.b['quarto_is_r_mode'] == nil then
+    vim.cmd('normal' .. slime_send_region_cmd)
+    return
+  end
+  if vim.b['quarto_is_r_mode'] == true then
+    vim.g.slime_python_ipython = 0
+    local is_python = require('otter.tools.functions').is_otter_language_context 'python'
+    if is_python and not vim.b['reticulate_running'] then
+      vim.fn['slime#send']('reticulate::repl_python()' .. '\r')
+      vim.b['reticulate_running'] = true
+    end
+    if not is_python and vim.b['reticulate_running'] then
+      vim.fn['slime#send']('exit' .. '\r')
+      vim.b['reticulate_running'] = false
+    end
+    vim.cmd('normal' .. slime_send_region_cmd)
+  end
+end
+
+vim.keymap.set({'n','i'},'<leader><C-p>',send_cell, { desc = 'send code cell to terminal' })
