655402fc2d2b5ce8d604b2834fc74d6e750ec981
ConicalDrupe <ChrisJacobBoon@gmail.com>
2024-04-18 20:58:36 -0400

    Added python load csv and path snippets

--- snippets/snippets.lua
+++ snippets/snippets.lua
@@ -19,6 +19,7 @@ local rep = require("luasnip.extras").rep
 -- ls.add_snippets(filetype, snippets)
 ls.add_snippets("all", {
   ls.parser.parse_snippet("expand", "-- this is what was expanded!"),
+  ls.parser.parse_snippet("OLS", "x_train, x_test , y_train, y_test = train_test_split($1 \n,$2 \n,test_size=0.20 \n,random_state=1) \n\nscaler = StandardScaler() \nx_train = scaler.fit_transform(x_train) \nx_test = scaler.transform(x_test) \n\nlinear_model = LinearRegression() \nlinear_model.fit(x_train,y_train)"),
 })
 
 ls.add_snippets("markdown", {
@@ -38,5 +39,9 @@ ls.add_snippets("python",{
   ls.parser.parse_snippet("exp_corr", "corr_fig = plt.figure() \ncorr_matrix = $1.corr() \ncorr_map = sns.heatmap(corr_matrix, annot = True)"),
   ls.parser.parse_snippet("exp_pair", "g = sns.PairGrid($1, corner=True) \ng.map_diag(sns.histplot) \ng.map_offdiag(sns.scotterplot)"),
   ls.parser.parse_snippet("OLS", "x_train, x_test , y_train, y_test = train_test_split($1 \n,$2 \n,test_size=0.20 \n,random_state=1) \n\nscaler = StandardScaler() \nx_train = scaler.fit_transform(x_train) \nx_test = scaler.transform(x_test) \n\nlinear_model = LinearRegression() \nlinear_model.fit(x_train,y_train)"),
+  ls.parser.parse_snippet("csv", "df = pd.read_csv(os.path.join(os.getcwd(),'$1.csv'))\n"),
+  ls.parser.parse_snippet("back_one_dir", "back_dir = os.path.normpath(os.getcwd() + os.sep + os.pardir)"),
+
+
   -- ls.parser.parse_snippet("evaluate","Code here"),
   })
