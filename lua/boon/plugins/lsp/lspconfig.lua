b0aca8d6200655b405071bd5a2bb16bdf729d1e4
ConicalDrupe <ChrisJacobBoon@gmail.com>
2024-07-28 14:36:17 -0400

    Added golang lsp

--- lua/boon/plugins/lsp/lspconfig.lua
+++ lua/boon/plugins/lsp/lspconfig.lua
@@ -141,6 +141,7 @@ return { -- LSP Configuration & Plugins
         -- r_language_server = {},
         sqlls = {},
         marksman = {},
+        gopls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
