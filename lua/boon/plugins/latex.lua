6cc9f74642ab1d8a0bcb07ae81bcfb62ac6ab41b
WSL-Home-Desktop <ChrisJacobBoon@gmail.com>
2024-12-30 22:15:39 -0500

    Added vimtex with zathura PDF viewer default

--- lua/boon/plugins/latex.lua
+++ lua/boon/plugins/latex.lua
@@ -6,4 +6,13 @@ return
             { '<leader>qm', ':lua require"nabla".toggle_virt()<cr>', desc = 'toggle [m]ath equations' },
         },
     },
+
+    {
+        "lervag/vimtex",
+        lazy = false,     -- we don't want to lazy load VimTeX
+        init = function()
+            -- VimTeX configuration goes here, e.g.
+            vim.g.vimtex_view_method = "zathura"
+        end
+    }
 }
