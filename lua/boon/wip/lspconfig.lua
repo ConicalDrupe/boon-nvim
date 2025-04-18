2dd5c4e0e6e3eadad51fdf1b48afa7e7d0935a43
WSL-Home-Desktop <ChrisJacobBoon@gmail.com>
2025-04-18 13:22:27 -0400

    Added clangd for C programming language

--- lua/boon/wip/lspconfig.lua
+++ lua/boon/wip/lspconfig.lua
@@ -142,6 +142,7 @@ return { -- LSP Configuration & Plugins
         sqlls = {},
         marksman = {},
         gopls = {},
+        clangd = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
