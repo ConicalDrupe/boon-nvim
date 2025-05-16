f903b4ff9b1da2890fec51dc4010a206ed6770fd
ConicalDrupe <ChrisJacobBoon@gmail.com>
2025-05-16 12:22:11 -0400

    renamed folder

--- ftplugin/quarto.lua
+++
@@ -1,5 +0,0 @@
--- will only be excecuted when we open a quarto file
-vim.b.slime_cell_delimiter = '```'
-
-vim.b['quarto_is_r_mode'] = nil
-vim.b['reticulate_running'] = false
--- ftplugin/yaml.lua
+++
@@ -1,3 +0,0 @@
--- Set yaml tabstop and shiftwidth for easier editing
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
