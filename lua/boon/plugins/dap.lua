bf8aaeb8156bd7b68e32b8a069b1d87a80c01812
ConicalDrupe <ChrisJacobBoon@gmail.com>
2025-09-28 14:24:53 -0400

    Made DAP F-keys more intuitive

--- lua/boon/plugins/dap.lua
+++ lua/boon/plugins/dap.lua
@@ -98,9 +98,9 @@ return {
       --   require("dapui").eval(nil, { enter = true })
       -- end)
 
+      vim.keymap.set("n", "<F1>", dap.step_over)
       vim.keymap.set("n", "<F2>", dap.continue)
-      vim.keymap.set("n", "<F1>", dap.step_into)
-      vim.keymap.set("n", "<F3>", dap.step_over)
+      vim.keymap.set("n", "<F3>", dap.step_into)
       vim.keymap.set("n", "<F4>", dap.step_out)
       vim.keymap.set("n", "<F5>", dap.step_back)
       vim.keymap.set("n", "<F13>", dap.restart)
