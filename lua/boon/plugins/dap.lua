9d6f80a009452190c006c453cff9d4064ffd3919
ConicalDrupe <ChrisJacobBoon@gmail.com>
2025-09-27 23:19:00 -0400

    Added python debugging

--- lua/boon/plugins/dap.lua
+++ lua/boon/plugins/dap.lua
@@ -2,16 +2,20 @@ return {
   {
     "mfussenegger/nvim-dap",
     dependencies = {
-      -- "rcarriga/nvim-dap-ui",
+      "mfussenegger/nvim-dap-python",
+      "rcarriga/nvim-dap-ui",
       -- "theHamsta/nvim-dap-virtual-text",
       "nvim-neotest/nvim-nio",
       "williamboman/mason.nvim",
     },
     config = function()
       local dap = require "dap"
-      -- local ui = require "dapui"
+      local ui = require "dapui"
 
-      -- require("dapui").setup()
+      require("dapui").setup()
+      require("dap-python").setup("python3")
+      -- require("dap-python").setup("uv") --if using uv
+      -- require("dap-python").test_runner = 'pytest' -- for pytest runner
 
     --  -- For Hiding API Keys on streaming
     --   require("nvim-dap-virtual-text").setup {
@@ -33,6 +37,7 @@ return {
     -- -- endOfhiding API keys
 
 
+      -- C debugger
       local c_ls_debugger = "gdb"--vim.fn.exepath "/usr/bin/gdb"
       if c_ls_debugger ~= "" then
         dap.adapters.gdb = {
@@ -93,25 +98,25 @@ return {
       --   require("dapui").eval(nil, { enter = true })
       -- end)
 
-      vim.keymap.set("n", "<F5>", dap.continue)
-      -- vim.keymap.set("n", "<F2>", dap.step_into)
-      -- vim.keymap.set("n", "<F3>", dap.step_over)
-      -- vim.keymap.set("n", "<F4>", dap.step_out)
-      -- -- vim.keymap.set("n", "<F5>", dap.step_back)
-      -- vim.keymap.set("n", "<F13>", dap.restart)
+      vim.keymap.set("n", "<F2>", dap.continue)
+      vim.keymap.set("n", "<F1>", dap.step_into)
+      vim.keymap.set("n", "<F3>", dap.step_over)
+      vim.keymap.set("n", "<F4>", dap.step_out)
+      vim.keymap.set("n", "<F5>", dap.step_back)
+      vim.keymap.set("n", "<F13>", dap.restart)
 
-      -- dap.listeners.before.attach.dapui_config = function()
-      --   ui.open()
-      -- end
-      -- dap.listeners.before.launch.dapui_config = function()
-      --   ui.open()
-      -- end
-      -- dap.listeners.before.event_terminated.dapui_config = function()
-      --   ui.close()
-      -- end
-      -- dap.listeners.before.event_exited.dapui_config = function()
-      --   ui.close()
-      -- end
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
     end,
   },
 }
