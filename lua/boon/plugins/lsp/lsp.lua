4db104d003e7ca613c08d3c72d5152d40a9b6881
ConicalDrupe <ChrisJacobBoon@gmail.com>
2026-05-02 16:44:42 -0500

    clang compiler flags for embedded projects

--- lua/boon/plugins/lsp/lsp.lua
+++ lua/boon/plugins/lsp/lsp.lua
@@ -177,7 +177,13 @@ return {
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				clangd = {},
+				clangd = {
+					cmd = {
+						"clangd",
+						"--background-index",
+						"--query-driver=/usr/bin/g++",
+					},
+				},
 				-- gopls = {},
 				basedpyright = {},
 				-- pyright = {},
