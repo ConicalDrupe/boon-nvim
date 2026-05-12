36f9434081b518b9e9e6de0e290cf31897db61fc
ConicalDrupe <ChrisJacobBoon@gmail.com>
2026-05-12 18:15:28 -0500

    Ignore $,' for Scala and Java files

--- lua/boon/plugins/autoclose.lua
+++ lua/boon/plugins/autoclose.lua
@@ -1,12 +1,12 @@
 return {
-    "m4xshen/autoclose.nvim",
-    opts = {
-        disabled_filetypes = {"text", "markdown"},
-        keys = {
-            ["$"] = {escape = true, close=true,pair ="$$", disabled_filetypes={}},
-            ["'"] = { escape = true, close = true, pair = "''", disabled_filetypes={"markdown"}},
-            ["`"] = { escape = true, close = false, pair = "```" },
-            ["*"] = { escape = true, close = false, pair = "**", enabled_filetypes={"markdown"} },
-        }
-    }
+	"m4xshen/autoclose.nvim",
+	opts = {
+		disabled_filetypes = { "text", "markdown" },
+		keys = {
+			["$"] = { escape = true, close = true, pair = "$$", disabled_filetypes = { "java", "scala" } },
+			["'"] = { escape = true, close = true, pair = "''", disabled_filetypes = { "markdown", "java", "scala" } },
+			["`"] = { escape = true, close = false, pair = "```" },
+			["*"] = { escape = true, close = false, pair = "**", enabled_filetypes = { "markdown" } },
+		},
+	},
 }
