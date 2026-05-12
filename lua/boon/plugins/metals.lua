1a3b3340741721cbb3c9b4c7dcc60afca9a6b888
ConicalDrupe <ChrisJacobBoon@gmail.com>
2026-05-12 18:14:49 -0500

    metals fixed for Scala and Java

--- lua/boon/plugins/metals.lua
+++ lua/boon/plugins/metals.lua
@@ -3,20 +3,31 @@ return {
 	dependencies = {
 		{ "nvim-lua/plenary.nvim" },
 		{ "j-hui/fidget.nvim" },
-		-- { "saghen/blink-cmp" },
+		-- { "saghen/blink.cmp" },
 	},
 	ft = { "scala", "sbt", "java" },
 	opts = function()
 		local metals_config = require("metals").bare_config()
-		metals_config.on_attach = function(client, bufnr)
-			-- your on_attach function
-			metals_config.settings = {
-				showImplicitArguments = true,
-				excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
-			}
 
-			metals_config.capabilities = require("blink.cmp").get_lsp_capabilities()
-			metals_config.init_options.statusBarProvider = "off"
+		-- Must be set here, BEFORE initialize_or_attach — not inside on_attach
+		metals_config.settings = {
+			showImplicitArguments = true,
+			excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+		}
+
+		metals_config.capabilities = require("blink.cmp").get_lsp_capabilities()
+
+		metals_config.init_options.statusBarProvider = "off"
+
+		-- on_attach fires after connection: use only for Metals-specific keymaps.
+		-- Standard LSP keymaps (gd, gr, K, etc.) are handled by lsp.lua's LspAttach autocmd.
+		metals_config.on_attach = function(client, bufnr)
+			local map = function(keys, func, desc, mode)
+				mode = mode or "n"
+				vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = "Metals: " .. desc })
+			end
+			map("<leader>mc", require("telescope").extensions.metals.commands, "[M]etals [C]ommands")
+			map("<leader>mh", require("metals").hover_worksheet, "[M]etals [H]over Worksheet")
 		end
 
 		return metals_config
